aa2faa8552ccc8759ab55cf6d5d567f29e7c8ade
mahesh2247 <maheshm.mca18@bmsce.ac.in>
2022-10-08 10:05:57 +0530

    adding code

---
+++ lambda/handler.py
@@ -0,0 +1,77 @@
+import logging
+import json
+import uuid
+import boto3
+import os
+
+LOG = logging.getLogger()
+LOG.setLevel(logging.INFO)
+
+
+def main(event, context):
+    LOG.info("EVENT: " + json.dumps(event))
+    query_string_params = event["queryStringParameters"]
+    if query_string_params is not None:
+        target_url = query_string_params['targetUrl']
+        if target_url is not None:
+            return create_short_url(event)
+
+    path_parameters = event['pathParameters']
+    if path_parameters is not None:
+        if path_parameters['proxy'] is not None:
+            return read_short_url(event)
+
+    return {
+        'statusCode': 200,
+        'body': 'usage: ?targetUrl=URL'
+    }
+
+def create_short_url(event):
+    table_name = os.environ.get('TABLE_NAME')
+
+    target_url = event['queryStringParameters']['targetUrl']
+
+    id = str(uuid.uuid4())[0:8]
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+    table.put_item(Item={
+        'id': id,
+        'target_url': target_url
+    })
+
+    url = "https://" \
+        + event["requestContext"]["domainName"] \
+        + event["requestContext"]["path"] \
+        + id
+
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'text/plain'},
+        'body': "Created URL: %s" % url
+    }
+
+
+def read_short_url(event):
+    id = event['pathParameters']['proxy']
+    table_name = os.environ.get('TABLE_NAME')
+
+    ddb = boto3.resource('dynamodb')
+    table = ddb.Table(table_name)
+    response = table.get_item(Key={'id': id})
+    LOG.debug("Response: " + json.dumps(response))
+
+    item = response.get('Item', None)
+    if item is None:
+        return {
+            'statusCode': 400,
+            'headers': {'Content-Type': 'text/plain'},
+            'body': 'No redirect found for ' + id
+        }
+
+    return {
+        'statusCode': 301,
+        'headers': {
+            'Location': item.get('target_url')
+        }
+    }
--- url_shortner/url_shortner_stack.py
+++ url_shortner/url_shortner_stack.py
@@ -1,8 +1,4 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import aws_dynamodb, aws_lambda, aws_apigateway, RemovalPolicy, Stack
 from constructs import Construct
 
 class UrlShortnerStack(Stack):
@@ -10,10 +6,18 @@ class UrlShortnerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        table = aws_dynamodb.Table(self, "mapping-table", partition_key=aws_dynamodb.Attribute(name="id", type=aws_dynamodb.AttributeType.STRING))
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "UrlShortnerQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+
+        function = aws_lambda.Function(self, "backend",
+                                            runtime=aws_lambda.Runtime.PYTHON_3_7,
+                                            handler="handler.main",
+                                            code=aws_lambda.Code.from_asset("./lambda"))
+
+
+        table.grant_read_write_data(function)
+        function.add_environment("TABLE_NAME", table.table_name)
+
+        api = aws_apigateway.LambdaRestApi(self, "api", handler=function)
+
+        table.apply_removal_policy(RemovalPolicy.DESTROY)
